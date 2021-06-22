fdede740cfd77d4a0862971f7ff59016e44fafac
Marcelo Sercheli <msercheli@gmail.com>
2021-06-22 17:26:22 -0400

    Remove cmake and gcc from rez and use devtoolset 6. Add FindOpenSubdiv.cmake from Pixar. Set to disable many options in CMakeLists

--- package.py
+++ package.py
@@ -29,16 +29,10 @@ with scope("config") as c:
         c.release_packages_path = os.environ["SSE_REZ_REPO_RELEASE_EXT"]
 
 requires = [
-    "gcc-6.3",
-    "tbb-2017.6",
-    "glfw-3.4.0",
-    "glew-2.0.0",
     "ptex-2.1.28",
 ]
 
 private_build_requires = [
-    "cmake",
-    "python-2",
 ]
 
 variants = [
@@ -49,9 +43,13 @@ build_system = "cmake"
 
 uuid = "repository.OpenSubdiv"
 
+def pre_build_commands():
+    command("source /opt/rh/devtoolset-6/enable")
+
 def commands():
     env.OPENSUBDIV_ROOT_DIR = "{root}"
     env.PATH.prepend("{root}/bin")
-    env.LD_LIBRARY_PATH.append("{root}/lib")
+    #env.LD_LIBRARY_PATH.append("{root}/lib")
+
     if building:
-        env.CMAKE_MODULE_PATH.append("{root}/cmake")
+        env.CMAKE_MODULE_PATH.append("{root}/rez_cmake")
